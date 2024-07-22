dfa1ba311090966db5c0627cee48b778115fd725
software <yasminvitorialinardi@gmail.com>
2024-07-22 11:45:46 -0300

    aaaaaaaaa

---
+++ software/Pessoa.cs
@@ -0,0 +1,64 @@
+namespace software;
+
+public class Pessoa
+{
+    string Nome;
+    string Telefone;
+    string CPF;
+    string Email;
+    string Endereço;
+    Int Id; //int64 serve para ter mais bits
+    
+    public void SetNome (string n)
+{
+    Nome = n;
+}
+
+public string GetNome ()
+{
+    return Nome;
+}
+
+public void SetTelefone (string t)
+{
+     Telefone = t;
+}
+
+public string GetTelefone ()
+{
+    return Telefone;
+}
+
+public void SetCPF (string c)
+{
+     CPF = c;
+}
+
+public string GetCPF ()
+{
+    return CPF;
+}
+
+
+public void SetEmail (string em)
+{
+     Email = em;
+}
+
+public string GetEmail ()
+{
+    return Email;
+}
+
+public void SetEndereço (string en)
+{
+     Endereço = en;
+}
+
+public string GetEndereço ()
+{
+    return Endereço;
+}
+
+
+}
