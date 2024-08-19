8f0ece65f127625c9328620c4048697b757b367b
software <yasminvitorialinardi@gmail.com>
2024-08-19 10:07:10 -0300

    9 atts

--- software/Telas/CadastrodeCliente.xaml.cs
+++ software/Telas/CadastrodeCliente.xaml.cs
@@ -4,7 +4,7 @@ using Microsoft.Maui.Controls;
 namespace software
 {
     public partial class CadastroClientePage : ContentPage
-{
+  {
    public Cliente cliente { get; set; }
   Controles.ClienteControle clienteControle = new Controles.ClienteControle();
 
@@ -31,21 +31,21 @@ namespace software
       SobrenomeEntry.Text = cliente.Sobrenome;
       TelefoneEntry.Text  = cliente.Telefone;
     }
-  }
+    }
 
   private void OnApagarDadosClicked(object sender, EventArgs e)
-  {
+    {
     IdLabel.Text = string.Empty;
     NomeEntry.Text = string.Empty;
     SobrenomeEntry.Text = string.Empty;
     TelefoneEntry.Text = string.Empty;
-  }
+    }
 
 
   private async void OnSalvarDadosClicked(object sender, EventArgs e)
-  {
-    if (await VerificaSeDadosEstaoCorretos())
     {
+    if (await VerificaSeDadosEstaoCorretos())
+      {
       var cliente = new Modelos.Cliente();
       if (!String.IsNullOrEmpty(IdLabel.Text))
         cliente.Id      = int.Parse(IdLabel.Text);
@@ -57,30 +57,30 @@ namespace software
 
       clienteControle.CriarOuAtualizar(cliente);
       await DisplayAlert("Salvar", "Dados salvos com sucesso!", "OK");
+      }
     }
-  }
 
   private async Task<bool> VerificaSeDadosEstaoCorretos()
-  {
+    {
     if (String.IsNullOrEmpty(NomeEntry.Text))
-    {
+      {
       await DisplayAlert("Cadastrar", "O campo Nome é obrigatório", "OK");
-      return false;
-    }
-    else if (String.IsNullOrEmpty(SobrenomeEntry.Text))
-    {
+        return false;
+      }
+      else if (String.IsNullOrEmpty(SobrenomeEntry.Text))
+      {
       await DisplayAlert("Cadastrar", "O campo Sobrenome é obrigatório", "OK");
-      return false;
-    }
-    else if (String.IsNullOrEmpty(TelefoneEntry.Text))
-    {
+        return false;
+      }
+      else if (String.IsNullOrEmpty(TelefoneEntry.Text))
+      {
       await DisplayAlert("Cadastrar", "O campo Telefone é obrigatório", "OK");
-      return false;
+        return false;
+      }
+      else
+        return true;
     }
-    else
-      return true;
-  }
 
-}
+  }
     }
-}
+
